caa92eb29ede62c637fdada0a4de470f61e8e57d
Madeleine Stewart <madeleine.stewart11@gmail.com>
2015-08-11 13:31:32 -0700

    unused import + container

--- app/src/main/java/me/karavaillancourt/wheelofeats/DetailsFragment.java
+++ app/src/main/java/me/karavaillancourt/wheelofeats/DetailsFragment.java
@@ -3,14 +3,12 @@ package me.karavaillancourt.wheelofeats;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
